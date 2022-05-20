237db0a393d0b5595bdc6f4f88fdc00c33bf1f33
JCaceres <jonca5995@gmail.com>
2022-05-20 20:51:02 +0200

    Creados componentes principales y hojas de estilo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NosotrosComponent } from './views/nosotros/nosotros.component';
+import { AyudaComponent } from './views/ayuda/ayuda.component';
+import { RoomComponent } from './views/room/room.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NosotrosComponent,
+    AyudaComponent,
+    RoomComponent,
   ],
   imports: [
     BrowserModule,
