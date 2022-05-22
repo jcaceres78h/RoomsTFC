95fc1d2f94cbab4d641e4cd35e498b100ba7ec71
Pedro Ballesta <pedroballesta1992@gmail.com>
2022-05-22 13:42:39 +0200

    Get User

--- src/app/listados/listar-users/listar-users.component.ts
+++ src/app/listados/listar-users/listar-users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/servicios/user/user.service';
 
 @Component({
   selector: 'app-listar-users',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarUsersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
   }
+
+  getUsers(){
+     console.log(this.userService.getAllUser())
+     return this.userService.getAllUser();
+   }
+
 
 }
