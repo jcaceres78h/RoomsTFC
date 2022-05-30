272c31201bbde5ed32c5c291cca88bf206d7ff7e
cincodes <79914945+Cintia-Andujar@users.noreply.github.com>
2022-05-30 14:00:36 +0200

    Responsive v1
    Header con menu hamburguesa, footer tarjetas resultados rooms

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,15 +8,32 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   mostrarPerfil = true
-
-  constructor() { }
-
+  mostrarNav = false
+  mostrarMenu = false
+  ancho:any
+  
+  constructor() {}
+  
   ngOnInit(): void {
-  }
 
+    //this.ancho = document.querySelector('.main_principal')?.clientWidth;
+    this.ancho = screen.width
+    if(this.ancho < 1280){
+      this.mostrarNav = true
+      const menu = document.querySelector('#logoBox')
+      menu?.classList.add('menu_responsive')
+    }else{
+      this.mostrarMenu = true
+      const menu = document.querySelector('#logoBox')
+      menu?.classList.remove('menu_responsive')
+    }
+    console.log(this.ancho)
+  }
+  
   mostrarPerfilCard(){
     this.mostrarPerfil = !this.mostrarPerfil
   }
+
 
 
 }
