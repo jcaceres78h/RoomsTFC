0845644953e72cf74fc736b9388e25e2dfd92943
JCaceres <jonca5995@gmail.com>
2022-06-13 00:41:48 +0200

    Añadidas fotos a tarjetas de usuario.

--- src/app/cards/user-card/user-card.component.ts
+++ src/app/cards/user-card/user-card.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FavoritosService } from 'src/app/servicios/favoritos/favoritos.service';
+import axios from 'axios';
+import { LoginService } from '../../servicios/login/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-card',
@@ -10,9 +13,14 @@ export class UserCardComponent implements OnInit {
 
   @Input() user : any
 
-  constructor(private favoritoService: FavoritosService) { }
+  constructor(private favoritoService: FavoritosService, private ls: LoginService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    if (!this.ls.isLoggeado) {
+      this.router.navigate(['iniciar-sesion'])
+    }
+    this.getFotos()
   }
 
   next(direction:string){
@@ -34,5 +42,20 @@ setFavorito(user:any): void {
 isFavorito(user:any){
   return this.favoritoService.getFavoritosUser().includes(user);
 }
+
+  // fotos: any
+  foto_perfil = '';
+  getFotos() {
+    axios.get(`http://167.99.46.205/index.php/api/images?type=user&id=${this.user.id}`)
+      .then(e => {
+        let foto = e.data.data[0]
+        // this.fotos = e.data.data;
+        this.foto_perfil = foto.file_path
+        this.foto_perfil = "background: url('http://167.99.46.205"+this.foto_perfil+"') no-repeat center; background-size: cover;"
+        console.log(this.foto_perfil)
+      })
+      .catch(e => {
+      })
+  }
 
 }
