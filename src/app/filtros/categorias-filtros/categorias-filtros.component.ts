8932a9c34c00b43196a570a0a9b5e0f03fc4b62f
JCaceres <jonca5995@gmail.com>
2022-06-12 12:31:49 +0200

    Añadidas las funciones a categorías filtros

--- src/app/filtros/categorias-filtros/categorias-filtros.component.ts
+++ src/app/filtros/categorias-filtros/categorias-filtros.component.ts
@@ -15,7 +15,7 @@ export class CategoriasFiltrosComponent implements OnInit {
   public card3 = false
   public card4 = false
 
-  constructor() {  
+  constructor() {
 
   }
 
@@ -55,8 +55,128 @@ export class CategoriasFiltrosComponent implements OnInit {
     this.card1 = false
   }
 
+  num_roommates = 0;
 
+  mas_roomMate() {
+    if (this.num_roommates < 10)
+      this.num_roommates++;
+  }
 
+  menos_roomMate() {
+    if (this.num_roommates > 0)
+      this.num_roommates--;
+  }
 
+  gender = 0;
+  selectGender(gender: number) {
+    this.gender = gender;
+  }
+
+  cama = 1;
+  selectCama(tipo: number) {
+    this.cama = tipo;
+  }
+
+  amueblada = false
+  changeAmueblada() {
+    this.amueblada = !this.amueblada;
+  }
+
+  banoPrivado = false
+  changeBanoPrivado() {
+    this.banoPrivado = !this.banoPrivado;
+  }
+
+  ventana = false
+  changeVentana() {
+    this.ventana = !this.ventana;
+  }
+
+  fumar = false
+  changeFumar() {
+    this.fumar = !this.fumar
+  }
+
+  mascotas = false
+  changeMascotas() {
+    this.mascotas = !this.mascotas
+  }
+
+  parejas = false
+  changeParejas() {
+    this.parejas = !this.parejas
+  }
+
+  wifi = false
+  changeWifi() {
+    this.wifi = !this.wifi
+  }
+
+  ascensor = false
+  changeAscensor() {
+    this.ascensor = !this.ascensor
+  }
+
+  lavadora = false
+  changeLavadora() {
+    this.lavadora = !this.lavadora
+  }
+
+  secadora = false
+  changeSecadora() {
+    this.secadora = !this.secadora
+  }
+
+  lavavajillas = false
+  changeLavavajillas() {
+    this.lavavajillas = !this.lavavajillas
+  }
+
+  jardin = false
+  changeJardin() {
+    this.jardin = !this.jardin
+  }
+
+  balcon = false
+  changeBalcon() {
+    this.balcon = !this.balcon
+  }
+
+  calefaccion = false
+  changeCalefaccion() {
+    this.calefaccion = !this.calefaccion
+  }
+
+  portero = false
+  changePortero() {
+    this.portero = !this.portero
+  }
+
+  accesible = false
+  changeAccesible() {
+    this.accesible = !this.accesible
+  }
+
+  parking = false
+  changeParking() {
+    this.parking = !this.parking
+  }
+
+  aireAcondicionado = false
+  changeAireAcondicionado() {
+    this.aireAcondicionado = !this.aireAcondicionado
+  }
+
+  presupuestoMinimo: number = 0
+  presupeustoMaximo: number = 99999
+
+  changeValorMinimo() {
+    console.log("Nuevo valor mínimo")
+    //Probando cambios
+  }
+
+  changeValorMaximo() {
+    console.log("Nuevo valor máximo")
+  }
 
 }
